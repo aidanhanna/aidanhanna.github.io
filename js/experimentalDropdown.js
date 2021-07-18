8a7b029460c578372c4ce77f6e3a896afc86975d
aidanhanna <84487262+aidanhanna@users.noreply.github.com>
2021-07-17 22:19:48 -0500

    Update experimentalDropdown.js

--- js/experimentalDropdown.js
+++ js/experimentalDropdown.js
@@ -3,7 +3,31 @@
  // closing of dropdowns //
 //////////////////////////
 
+// used to store dropdown states
 let aboutVisible = false;
+let edVisible = false;
+let expVisible = false;
+let intVisible = false;
+
+function closeAll() {
+   if (aboutVisible)
+   {
+      toggleAbout();
+   }
+   if (edVisible) 
+   {
+      toggleEd();
+   }
+   if (expVisible)
+   {
+      toggleExp();
+   }
+   if (intVisible)
+   {
+      toggleInt();
+   }
+}
+
 function toggleAbout() {
    let dropdown = document.querySelector("#about-list");
    /*dropdown.style.height = "500px";*/
@@ -14,12 +38,12 @@ function toggleAbout() {
    }
    else
    {
+      closeAll();
       dropdown.style.height = "auto";
       aboutVisible = true;
    }
 }
 
-let edVisible = false;
 function toggleEd() {
    let dropdown = document.querySelector("#ed-list");
    /*dropdown.style.height = "500px";*/
@@ -30,12 +54,12 @@ function toggleEd() {
    }
    else
    {
+      closeAll();
       dropdown.style.height = "auto";
       edVisible = true;
    }
 }
 
-let expVisible = false;
 function toggleExp() {
    let dropdown = document.querySelector("#exp-list");
    /*dropdown.style.height = "500px";*/
@@ -46,12 +70,12 @@ function toggleExp() {
    }
    else
    {
+      closeAll();
       dropdown.style.height = "auto";
       expVisible = true;
    }
 }
 
-let intVisible = false;
 function toggleInt() {
    let dropdown = document.querySelector("#int-list");
    /*dropdown.style.height = "500px";*/
@@ -62,10 +86,12 @@ function toggleInt() {
    }
    else
    {
+      closeAll();
       dropdown.style.height = "auto";
       intVisible = true;
    }
 }
+
 
    ////////////////////////////
   // controls changing view //
@@ -78,39 +104,56 @@ window.addEventListener("resize", checkSpace, false);
 let out = document.querySelector(".output");
   
 function checkSpace() {
+   // calculate widths
    let windowWidth = document.body.clientWidth;
    let buttonBarWidth = 890;
    
-   if (buttonBarWidth > windowWidth)
+   // compare widths
+   if (buttonBarWidth > windowWidth) // if too small
    {
+      // reformat main buttons to collapse to the side
       let dropdowns = document.querySelectorAll(".header-button");
       dropdowns.forEach(function(dropdown) { 
          dropdown.style.height = "0px";
          dropdown.style.display = "block";
          dropdown.style.width = "100vw";
+         dropdown.style.position = "absolute";
       })
       
+      let lists = document.querySelectorAll(".button-list");
+      lists.forEach(function(list) { 
+         list.style.height = "0px";
+         list.style.display = "block";
+         list.style.width = "100vw";
+      })
+      
+      // show toggle
       let menuToggle = document.querySelector(".menu-toggle");
       menuToggle.style.width = "50px";
    }
    else
    {
+      // close menu if necessary
       if (menuVisible)
       {
          toggleMenu();
       }
       
+      // reformat main buttons into a line
       let dropdowns = document.querySelectorAll(".header-button");
       dropdowns.forEach(function(dropdown) { 
          dropdown.style.display = "inline-block";
          dropdown.style.height = "50px";
       })
+      
+      // reset button widths
       document.querySelector("#about-button").style.width = "160px";
       document.querySelector("#education-button").style.width = "240px";
       document.querySelector("#experience-button").style.width = "250px";
       document.querySelector("#interest-button").style.width = "240px";
-     
-      /*document.querySelector("#about-button").style.width = "160px";*/
+      
+      // reset horizontal positions for lists
+      // about list is always at 0
       document.querySelector("#ed-list").style.left = "160px";
       document.querySelector("#exp-list").style.left = "400px";
       document.querySelector("#int-list").style.left = "650px";
