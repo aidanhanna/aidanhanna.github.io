76d319ad89a08c7c894af30a5ddb1801e8d94924
aidanhanna <84487262+aidanhanna@users.noreply.github.com>
2021-07-29 19:15:13 -0500

    Create newDropdown.js

---
+++ js/newDropdown.js
@@ -0,0 +1,132 @@
+   //////////////////////////
+  // controls opening and //
+ // closing of dropdowns //
+//////////////////////////
+
+let aboutVisible = false;
+function toggleAbout() {
+   let dropdown = document.querySelector("#about-list");
+   if (aboutVisible)
+   {
+      dropdown.style.height = "0px";
+      aboutVisible = false;
+   }
+   else
+   {
+      dropdown.style.height = "auto";
+      aboutVisible = true;
+   }
+}
+
+let edVisible = false;
+function toggleEd() {
+   let dropdown = document.querySelector("#ed-list");
+   if (edVisible)
+   {
+      dropdown.style.height = "0px";
+      edVisible = false;
+   }
+   else
+   {
+      dropdown.style.height = "auto";
+      edVisible = true;
+   }
+}
+
+let expVisible = false;
+function toggleExp() {
+   let dropdown = document.querySelector("#exp-list");
+   if (expVisible)
+   {
+      dropdown.style.height = "0px";
+      expVisible = false;
+   }
+   else
+   {
+      dropdown.style.height = "auto";
+      expVisible = true;
+   }
+}
+
+let intVisible = false;
+function toggleInt() {
+   let dropdown = document.querySelector("#int-list");
+   if (intVisible)
+   {
+      dropdown.style.height = "0px";
+      intVisible = false;
+   }
+   else
+   {
+      dropdown.style.height = "auto";
+      intVisible = true;
+   }
+}
+
+   ////////////////////////////
+  // controls changing view //
+ // if window is too small //
+////////////////////////////
+
+window.addEventListener("load", checkSpace, false);
+window.addEventListener("resize", checkSpace, false);
+                  
+let out = document.querySelector(".output");
+  
+function checkSpace() {
+   let windowWidth = document.body.clientWidth;
+   let buttonBarWidth = 890;
+   
+   if (buttonBarWidth > windowWidth)
+   {
+     /*
+      let dropdowns = document.querySelectorAll(".header-button");
+      dropdowns.forEach(function(dropdown) { 
+         dropdown.style.height = "0px";
+         dropdown.style.display = "block";
+         dropdown.style.width = "100vw";
+      })
+      
+      let menuToggle = document.querySelector(".menu-toggle");
+      menuToggle.style.width = "50px";
+      */
+   }
+   else
+   {
+      /*
+      if (menuVisible)
+      {
+         toggleMenu();
+      }
+      
+      let dropdowns = document.querySelectorAll(".header-button");
+      dropdowns.forEach(function(dropdown) { 
+         dropdown.style.display = "inline-block";
+         dropdown.style.height = "50px";
+      })
+      
+      let menuToggle = document.querySelector(".menu-toggle");
+      menuToggle.style.width = "0px";
+      */
+     
+      document.querySelector("#about-button").style.width = "160px";
+      document.querySelector("#education-button").style.width = "240px";
+      document.querySelector("#experience-button").style.width = "250px";
+      document.querySelector("#interest-button").style.width = "240px";
+   }
+   
+   out.textContent = document.querySelector("#about-button").querySelector("button-text").style.width;
+}
+
+let menuVisible = false;
+function toggleMenu() {
+   if (menuVisible)
+   {
+     
+   }
+   else 
+   {
+      
+   }
+}
+
