c0b865dca578b573694ef2b63092965650d617e8
aidanhanna <84487262+aidanhanna@users.noreply.github.com>
2021-08-12 16:18:56 -0500

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -1,2 +1,29 @@
-//
+// This file is used to control collapsing 
+// and expanding semesters and courses on 
+// the class page 
 
+let courses = {F20: { "semester": false, "ECSE1010": false, "CSCI1100": false, "IHSS1972": false, "MATH2010": false, "MATH2400": false },
+               S21: { "semester": false, "ECSE1010": false, "CSCI1100": false, "IHSS1972": false, "MATH2010": false, "MATH2400": false },
+               F21: { "semester": false, "ECSE1010": false, "CSCI1100": false, "IHSS1972": false, "MATH2010": false, "MATH2400": false },
+               S22: { "semester": false}};
+              
+
+//semester is formatted like 'F20'
+//id is the HTML id NOT including '#'
+//type is either 'course' or 'semester'
+function toggleCourses(semester, id, type) {
+  if (type === 'course')
+  {
+    //find course to toggle
+    let course = document.querySelector("#" + id)
+    
+    if (courses[semester][id])
+    {
+      course.style.height = "0px";
+    }
+    else
+    {
+      course.style.height = "auto";
+    }
+  }
+}
