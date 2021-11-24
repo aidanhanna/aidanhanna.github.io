67cbbb236f3ea1a4ee2935194e767253afc67770
aidanhanna <84487262+aidanhanna@users.noreply.github.com>
2021-11-23 23:21:56 -0500

    Update chess.js

--- js/chess.js
+++ js/chess.js
@@ -1,1 +1,24 @@
+  ///////////////////////
+ // STORE BOARD STATE //
+///////////////////////
 
+let board = {A: {1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null}}
+
+let selected = [null, null];
+
+
+function selectSquare(col, row) {
+  //if no piece currently selected
+  if (selected == [null, null])
+  {
+    selected = [col, row]
+  }
+  
+  //if valid piece IS currently selected
+  else
+  {
+    
+  }
+  let clicked = [col, row]
+  
+}
