6362d8d4947b1cf98de1533e196fbed62c285310
aidan <84487262+aidanhanna@users.noreply.github.com>
2024-01-09 19:46:28 -0500

    Update class page to include all coursework

--- js/classes.js
+++ js/classes.js
@@ -4,67 +4,72 @@
 
 //stores visibility of each class and semester
 //'true' is visible, 'false' is hidden
-let courses = {"F20": { "semester": false, "ECSE1010": false, "CSCI1100": false, "IHSS1972": false, "MATH2010": false, "MATH2400": false },
-               "S21": { "semester": false, "ECSE2010": false, "ENGR2350": false, "CSCI1200": false, "ECSE2500": false, "PHYS1200": false },
-               "F21": { "semester": false, "ECSE4480": false, "ECSE2610": false, "ECSE2210": false, "ECSE2100": false, "CSCI2200": false, "ENGR1400": false },
-               "S22": { "semester": false}};
+let courses = {
+    // Fall 2020
+    "CSCI1100": false,
+    "ECSE1010": false,
+    "IHSS1972": false, 
+    "MATH2010": false,
+    "MATH2400": false,
+
+    // Spring 2021
+    "CSCI1200": false,
+    "ECSE2010": false, 
+    "ECSE2500": false,
+    "ENGR2350": false, 
+    "PHYS1200": false,
+
+    // Fall 2021
+    "CSCI2200": false,
+    "ECSE2100": false,
+    "ECSE2210": false,
+    "ECSE2610": false,
+    "ECSE4480": false,
+    "ENGR1400": false,
+
+    // Spring 2022
+    "ADMN1030": false,
+    "COGS2120": false,
+    "COMM2520": false,
+    "CSCI2300": false,
+    "ECSE2660": false,
+    "ECSE2900": false,
+    "ENGR4010": false,
+
+    // Summer 2022
+    "CSCI4210": false,
+    "ECSE2110": false,
+    "ECSE2410": false,
+    "ENGR2050": false,
+
+    // Spring 2023
+    "COGS4880": false,
+    "CSCI4380": false,
+    "ECSE2050": false,
+    "ECSE4220": false,
+    "STSO4100": false,
+
+    // Fall 2023
+    "COGS4420": false,
+    "ECSE4900": false
+}
               
 
-//semester is formatted like 'F20'
 //id is the HTML id NOT including '#'
-//type is either 'course' or 'semester'
-function toggleCourses(semester, id, type) {
+function toggleCourse(id) {
   
-  let out = document.querySelector(".output");
-  //out.textContent = "#" + id + ", " + courses[semester][id] + ", " + courses[semester]["semester"];
-  
-  //for toggling a single class
-  if (type == 'course')
-  {
-    //find course to toggle
     let course = document.querySelector("#" + id);
     
-    //set height to change visibility
-    if (courses[semester][id])
-    {
-      course.style.height = "0px";
-    }
-    else
-    {
-      course.style.height = "auto";
-    }
+  //set height to change visibility
+  if (courses[id])
+  {
+    course.style.height = "0px";
+  }
+  else
+  {
+    course.style.height = "auto";
+  }
     
     //update state
-    courses[semester][id] = !courses[semester][id];
-  }
-  
-  //for toggling a whole semester
-  else if (type == 'semester')
-  {    
-    //find semester to toggle
-    let sem = document.querySelector("#" + id);
-    
-    //set height to change visibility
-    if (courses[semester]["semester"])
-    {
-      sem.style.height = "0px";
-      
-      //hide all classes 
-      /*
-      for (let courseID in courses[semester])
-      {
-        let course = document.querySelector("#" + id);
-        course.style.height = "0px";
-        courses[semester][courseID] = !courses[semester][courseID];
-      }
-      */
-    }
-    else
-    {
-      sem.style.height = "auto";
-    }
-    
-    //update state
-    courses[semester]["semester"] = !courses[semester]["semester"];
-  }
+    courses[id] = !courses[id];
 }
